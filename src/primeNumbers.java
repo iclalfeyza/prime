3cc50760680448e8f8c5f7002685a90bca0d24b5
iclal feyza <iclalfeyzaozeken@hotmail.com>
2022-11-22 21:28:06 +0300

    Initial commit

---
+++ src/primeNumbers.java
@@ -0,0 +1,11 @@
+public class prime {
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 100; i++) {
+            boolean primes = true;
+            for (int n = 2; n < i; n++) {
+                if ()
+            }
+        }
+    }
+}
